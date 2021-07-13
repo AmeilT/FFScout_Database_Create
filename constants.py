ac06485cb138e5b7cdfc24492f9a68a0e7a84983
Ameil <ameiltailor@outlook.com>
2021-07-13 15:55:27 +0100

    Initial commit

---
+++ constants.py
@@ -0,0 +1,3 @@
+seasons=[2011,2012,2013,2014,2015,2016,2017,2018,2019,2020]
+tables=["involvement","distribution","goalthreat","defending","setpieces","kpi_attacking","kpi_defending"]
+datafilepath="data"
